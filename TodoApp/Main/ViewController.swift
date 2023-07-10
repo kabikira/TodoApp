7d7768209a74742d42b4b956b33efe63e3ff363c
kabikira <kabikirar2@protonmail.com>
2023-07-11 08:25:33 +0900

    TodoEdit,TodoList追加

---
+++ TodoApp/Main/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  TodoApp
+//
+//  Created by koala panda on 2023/07/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet private weak var registerEmailTextField: UITextField!
+    @IBOutlet private weak var registerPasswordTextField: UITextField!
+    @IBOutlet private weak var registerNameTextField: UITextField!
+    @IBOutlet private weak var loginPasswordTextField: UITextField!
+    @IBOutlet private weak var loginEmailTextField: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    @IBAction private func tapRegisterButton(_ sender: Any) {
+    }
+
+    @IBAction private func tapLoginButton(_ sender: Any) {
+    }
+
+
+
+}
+
---
+++ TodoApp/TodoList/TodoListCell.swift
@@ -0,0 +1,25 @@
+//
+//  TodoListCell.swift
+//  TodoApp
+//
+//  Created by koala panda on 2023/07/09.
+//
+
+import UIKit
+
+final class TodoListCell: UITableViewCell {
+
+    static var className: String { String(describing: TodoListCell.self)}
+
+    @IBOutlet private weak var titleLabel: UILabel!
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        self.titleLabel.text = nil
+    }
+
+//    func configure(qiitaModel: QiitaModel) {
+//        self.tatleLabel.text = qiitaModel.title
+//    }
+
+}
