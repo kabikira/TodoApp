0de9af745c92484417211f12aa1fb3fb6dd630c3
kabikira <kabikirar2@protonmail.com>
2023-07-11 08:52:06 +0900

    MainstoryboardとViewControllerの紐付け

--- TodoApp/Main/ViewController.swift
+++ TodoApp/Main/ViewController.swift
@@ -12,20 +12,17 @@ class ViewController: UIViewController {
     @IBOutlet private weak var registerEmailTextField: UITextField!
     @IBOutlet private weak var registerPasswordTextField: UITextField!
     @IBOutlet private weak var registerNameTextField: UITextField!
-    @IBOutlet private weak var loginPasswordTextField: UITextField!
     @IBOutlet private weak var loginEmailTextField: UITextField!
-
+    @IBOutlet private weak var loginPasswordTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+
     @IBAction private func tapRegisterButton(_ sender: Any) {
     }
 
     @IBAction private func tapLoginButton(_ sender: Any) {
     }
-
-
-
 }
 
